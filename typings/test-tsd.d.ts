47e49c95bd62aac4507c4fc8970a154444793efe
Vadim Macagon <vadim.macagon@gmail.com>
2016-10-10 17:09:54 +0700

    Clear out old type definitions

--- typings/test-tsd.d.ts
+++
@@ -1,5 +0,0 @@
-/// <reference path="bunyan-prettystream/bunyan-prettystream.d.ts" />
-/// <reference path="chai/chai.d.ts" />
-/// <reference path="chai-as-promised/chai-as-promised.d.ts" />
-/// <reference path="mocha/mocha.d.ts" />
-/// <reference path="node/node.d.ts" />
